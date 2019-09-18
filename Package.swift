30b184cb0ad779ca9c4ac83ea7e569a86e261f8f
Marko Rankovic <markocrush96@gmail.com>
2019-09-18 21:55:03 +0100

    version not regarded

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
             targets: ["RollswhereEngine"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/markorankovic/Smorgasbord", from: "1.0.0")
+        .package(path: "https://github.com/markorankovic/Smorgasbord")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
